ee6cc3708c4549f74f39370342ae02ac0f72eba2
awl03 <awl03@cf866699-8164-4729-ac43-5d845946804a>
2010-12-02 19:06:28 +0000

    Fix case where a statement that forces a block to close is followed by a statement that forces a block to open.

--- src/uk/ac/ic/doc/cfg/model/scope/BodyScope.java
+++ src/uk/ac/ic/doc/cfg/model/scope/BodyScope.java
@@ -23,14 +23,15 @@ public class BodyScope extends ScopeWithParent {
 
 		for (int i = 0; i < nodes.length; i++) {
 
+			boolean knitNewBlock = false;
+
 			// If there are more statements in this body and the statement we
 			// just processed had multiple escape edges, the processing of the
 			// next statement must start at a new basic block and the
 			// fallthrough escape edges must link to it.
 			if (lastStatement.isEndOfBlock()) {
-				BasicBlock nextBlock = newBlock();
-				setCurrentBlock(nextBlock);
-				lastStatement.linkFallThroughsTo(nextBlock);
+				setCurrentBlock(null);
+				knitNewBlock = true;
 			}
 
 			previousStatement = lastStatement;
@@ -41,22 +42,29 @@ public class BodyScope extends ScopeWithParent {
 			if (bodyRoot == null) {
 				bodyRoot = lastStatement.getRoot();
 			}
+
+			// It's possible we didn't have a current block. If so, we pretend
+			// we did for the rest of the processing
 			if (getCurrentBlock() == null) {
 				setCurrentBlock(lastStatement.getRoot());
 			}
+
+			// if the previous statement ended the block, the subscope will have
+			// created a new block which we then set as our current block
+			assert !previousStatement.isEndOfBlock()
+					|| getCurrentBlock() == lastStatement.getRoot();
 
 			// The statement processor may have added to the current basic block
 			// or it may have started a new one. We can detect this by comparing
 			// the root it returns with the current block. If they don't match
 			// then the processor started a new basic block. In this case we
 			// must link the current block to the new root.
-			if (getCurrentBlock() != lastStatement.getRoot()) {
-				assert previousStatement.getFallthroughQueue().size() == 1;
-				
+			if (knitNewBlock || getCurrentBlock() != lastStatement.getRoot()) {
 				previousStatement.linkFallThroughsTo(lastStatement);
 				setCurrentBlock(lastStatement.getRoot());
+				knitNewBlock = false;
 			}
-			
+
 			// Union all statements' breakouts as we don't process them at this
 			// level
 			body.getBreakoutQueue().addAll(lastStatement.getBreakoutQueue());
@@ -65,9 +73,9 @@ public class BodyScope extends ScopeWithParent {
 		// Only push up last statement's fallthroughs as the others are
 		// dealt with by the body processing loop here
 		body.getFallthroughQueue().addAll(lastStatement.getFallthroughQueue());
-		
+
 		body.setRoot(bodyRoot);
-		
+
 		assert body.exitSize() > 0;
 		return body;
 	}
