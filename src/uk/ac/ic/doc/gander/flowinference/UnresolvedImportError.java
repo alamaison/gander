a2b2d5ac0b3921cce5bb1b8b95b84fae600e94c5
awl03 <awl03@cf866699-8164-4729-ac43-5d845946804a>
2011-03-10 11:45:52 +0000

    Commit missing file.

---
+++ src/uk/ac/ic/doc/gander/flowinference/UnresolvedImportError.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package uk.ac.ic.doc.gander.flowinference;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import uk.ac.ic.doc.gander.model.Package;
+
+// TODO: Change to Exception to force explicit handling
+public class UnresolvedImportError extends Error {
+
+	private static final long serialVersionUID = -3845956678785948260L;
+	private List<String> importPath;
+	private Package relativeToPackage;
+
+	public UnresolvedImportError(List<String> importPath,
+			Package relativeToPackage) {
+		this.importPath = importPath;
+		this.relativeToPackage = relativeToPackage;
+	}
+
+	@Override
+	public String getMessage() {
+		return "'" + join(importPath, ".")
+				+ "' couldn't be resolved from package '"
+				+ relativeToPackage.getFullName() + "'";
+	}
+
+	private static String join(Collection<?> s, String delimiter) {
+		StringBuilder builder = new StringBuilder();
+		Iterator<?> iter = s.iterator();
+		while (iter.hasNext()) {
+			builder.append(iter.next());
+			if (!iter.hasNext()) {
+				break;
+			}
+			builder.append(delimiter);
+		}
+		return builder.toString();
+	}
+
+}
