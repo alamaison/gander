67c970a5a949206ffc9ebd89e2c47b46ac179a67
awl03 <awl03@cf866699-8164-4729-ac43-5d845946804a>
2011-03-11 13:57:59 +0000

    Refactor on-disk model into hierarchy namespace.

---
+++ src/uk/ac/ic/doc/gander/hierarchy/build/BuildablePackage.java
@@ -0,0 +1,35 @@
+package uk.ac.ic.doc.gander.hierarchy.build;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import uk.ac.ic.doc.gander.hierarchy.Module;
+import uk.ac.ic.doc.gander.hierarchy.Package;
+
+class BuildablePackage extends Package {
+
+	private HashMap<String, Module> modules = new HashMap<String, Module>();
+	private HashMap<String, Package> packages = new HashMap<String, Package>();
+
+	public BuildablePackage(String name, File initFile, Package parent) {
+		super(name, initFile, parent);
+	}
+
+	public Map<String, Package> getPackages() {
+		return Collections.unmodifiableMap(packages);
+	}
+
+	public Map<String, Module> getModules() {
+		return Collections.unmodifiableMap(modules);
+	}
+
+	public void addPackage(Package subpackage) {
+		packages.put(subpackage.getName(), subpackage);
+	}
+
+	public void addModule(Module submodule) {
+		modules.put(submodule.getName(), submodule);
+	}
+}
