4437efe55fdc1af54ef9a352ddc3ff65fe4aa5b4
Alexander Lamaison <awl03@doc.ic.ac.uk>
2012-02-26 02:23:38 +0000

    Cleaned up ParameterTypeGoal.

--- gander/src/uk/ac/ic/doc/gander/flowinference/sendersgoals/FunctionSendersGoal.java
+++ gander/src/uk/ac/ic/doc/gander/flowinference/sendersgoals/FunctionSendersGoal.java
@@ -14,11 +14,18 @@ import uk.ac.ic.doc.gander.flowinference.flowgoals.FlowGoal;
 import uk.ac.ic.doc.gander.flowinference.result.FiniteResult;
 import uk.ac.ic.doc.gander.flowinference.result.Result;
 import uk.ac.ic.doc.gander.flowinference.result.Result.Processor;
+import uk.ac.ic.doc.gander.flowinference.types.TFunction;
 import uk.ac.ic.doc.gander.model.ModelSite;
 import uk.ac.ic.doc.gander.model.codeobject.InvokableCodeObject;
 
 /**
  * Find any callsites that could call the given function.
+ * 
+ * TODO: This should take a {@link TFunction} rather than a code object and it
+ * should return senders in the form of objects at call-site rather than just
+ * object. Code object arrive at a call-site as part of a callable object such
+ * as a bound method or the constructor of a class. We need to improve the flow
+ * modelling to track that rather than just code object flow.
  */
 public class FunctionSendersGoal implements SendersGoal {
 	private final InvokableCodeObject callable;
@@ -27,10 +34,12 @@ public class FunctionSendersGoal implements SendersGoal {
 		this.callable = callable;
 	}
 
+	@Override
 	public Result<ModelSite<Call>> initialSolution() {
 		return FiniteResult.bottom();
 	}
 
+	@Override
 	public Result<ModelSite<Call>> recalculateSolution(
 			final SubgoalManager goalManager) {
 		return new FunctionSendersGoalSolver(callable, goalManager).solution();
@@ -82,10 +91,12 @@ final class FunctionSendersGoalSolver implements Processor<ModelSite<exprType>> 
 		callableObjectPositions.actOnResult(this);
 	}
 
+	@Override
 	public void processInfiniteResult() {
 		callSites = TopS.INSTANCE;
 	}
 
+	@Override
 	public void processFiniteResult(Set<ModelSite<exprType>> positions) {
 		Set<ModelSite<Call>> callSitePositions = new HashSet<ModelSite<Call>>();
 
--- gander/src/uk/ac/ic/doc/gander/flowinference/typegoals/ParameterTypeGoal.java
+++ gander/src/uk/ac/ic/doc/gander/flowinference/typegoals/ParameterTypeGoal.java
@@ -1,5 +1,6 @@
 package uk.ac.ic.doc.gander.flowinference.typegoals;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.python.pydev.parser.jython.ast.Call;
@@ -14,6 +15,7 @@ import uk.ac.ic.doc.gander.flowinference.result.FiniteResult;
 import uk.ac.ic.doc.gander.flowinference.result.RedundancyEliminator;
 import uk.ac.ic.doc.gander.flowinference.result.Result;
 import uk.ac.ic.doc.gander.flowinference.result.Result.Transformer;
+import uk.ac.ic.doc.gander.flowinference.result.Top;
 import uk.ac.ic.doc.gander.flowinference.sendersgoals.FunctionSendersGoal;
 import uk.ac.ic.doc.gander.flowinference.types.TCallable;
 import uk.ac.ic.doc.gander.flowinference.types.Type;
@@ -98,9 +100,10 @@ final class ParameterTypeGoal implements TypeGoal {
 
 final class ParameterTypeGoalSolver {
 
-	private final Result<Type> solution;
-	private final SubgoalManager goalManager;
+	private final InvokableCodeObject invokable;
 	private final Variable variable;
+	private final SubgoalManager goalManager;
+	private final Result<Type> solution;
 
 	ParameterTypeGoalSolver(InvokableCodeObject invokable, Variable variable,
 			SubgoalManager goalManager) {
@@ -109,24 +112,112 @@ final class ParameterTypeGoalSolver {
 				variable);
 		assert goalManager != null;
 
+		this.invokable = invokable;
 		this.goalManager = goalManager;
 		this.variable = variable;
 
 		Result<ModelSite<Call>> callSites = goalManager
 				.registerSubgoal(new FunctionSendersGoal(invokable));
 
-		solution = deriveParameterTypeFromCallsites(invokable, callSites);
+		solution = deriveParameterTypeFromCallsites(callSites);
+	}
+
+	private Result<Type> processSender(ModelSite<Call> senderCallSite) {
+
+		/*
+		 * The FunctionSendersGoal has told us which expressions it thinks our
+		 * parameter's code object has flown to but that's only half the story.
+		 * It hasn't told us what callable object is flows there in (yes, we
+		 * should probably change the flow goals to better model this but, for
+		 * the moment, that's how it is) so we have to use type inference to get
+		 * the types as these affect the way arguments are passed to this code
+		 * object.
+		 * 
+		 * TODO: deal with this by tracking the type using the flow goal model
+		 */
+
+		Result<Type> callSiteTypes = inferObjectsCalledAtCallSite(senderCallSite);
+
+		/*
+		 * But now there may be all sorts of other objects in here that don't
+		 * invoke our code object when they are called. For example, if the
+		 * callsite that we tracked our code object's flow to is polymorphic and
+		 * also invokes other code objects.
+		 * 
+		 * We filter those out now.
+		 */
+		Result<TCallable> callables = filterOutUnrelatedObjects(callSiteTypes);
+
+		/*
+		 * Finally we can get on to inferring the type of the parameter. This
+		 * involved a complex little dance between the arguments at the
+		 * callsite, the type of object being called and the parameters of the
+		 * code object. They each contribute something to the negotiation that
+		 * leads to values flowing from the call-site to variables in the code
+		 * object. The code object could be called multiple ways at the same
+		 * call-site and each call affects the parameter type differently so the
+		 * code object receiver and the object it is being called as part of are
+		 * packaged up along with the call-site itself (for its arguments) as a
+		 * Call object that takes care of coordinating the dance.
+		 */
+		Result<uk.ac.ic.doc.gander.flowinference.call.Call> calls = packageCalls(
+				callables, senderCallSite);
+
+		/*
+		 * Dance.
+		 */
+		return deriveParameterTypeFromCalls(calls);
+	}
+
+	/**
+	 * Transforms a call-site into the objects that are called at it.
+	 * 
+	 * Which argument is passed to a parameter depends on how its code object is
+	 * invoked by the call site. For example, if the callable is a bound method,
+	 * the arguments are passed to different parameters than if it is used
+	 * unbound. This is decided by {@link Type} being called at the call site
+	 * (which might not call the code object in question).
+	 */
+	private Result<Type> inferObjectsCalledAtCallSite(ModelSite<Call> callSite) {
+
+		return goalManager.registerSubgoal(new ExpressionTypeGoal(
+				new ModelSite<exprType>(callSite.astNode().func, callSite
+						.codeObject())));
+	}
+
+	private Result<TCallable> filterOutUnrelatedObjects(
+			Result<Type> callSiteTypes) {
+
+		return callSiteTypes.transformResult(new CalledObjectFilter(invokable,
+				goalManager));
+	}
+
+	private Result<uk.ac.ic.doc.gander.flowinference.call.Call> packageCalls(
+			Result<TCallable> callables, final ModelSite<Call> senderCallSite) {
+
+		return callables.transformResult(new CallPackager(invokable,
+				senderCallSite));
+	}
+
+	private Result<Type> deriveParameterTypeFromCalls(
+			Result<uk.ac.ic.doc.gander.flowinference.call.Call> calls) {
+
+		return calls.transformResult(new CallDancer(variable, goalManager));
 	}
 
 	private Result<Type> deriveParameterTypeFromCallsites(
-			InvokableCodeObject invokable, Result<ModelSite<Call>> callSites) {
+			Result<ModelSite<Call>> callSites) {
 
 		Concentrator<ModelSite<Call>, Type> processor = Concentrator
-				.newInstance(new CallArgumentTyper(invokable, variable,
-						goalManager), TopT.INSTANCE);
+				.newInstance(new DatumProcessor<ModelSite<Call>, Type>() {
+
+					@Override
+					public Result<Type> process(ModelSite<Call> senderCallSite) {
+						return processSender(senderCallSite);
+					}
+				}, TopT.INSTANCE);
 
 		callSites.actOnResult(processor);
-
 		return processor.result();
 	}
 
@@ -136,83 +227,107 @@ final class ParameterTypeGoalSolver {
 }
 
 /**
- * Transforms a call-site into the types that may be bound to the given
- * variable.
+ * Transforms a call-site into the objects that are called at it.
+ * 
+ * Which argument is passed to a parameter depends on how its code object is
+ * invoked by the call site. For example, if the callable is a bound method, the
+ * arguments are passed to different parameters than if it is used unbound. This
+ * is decided by {@link Type} being called at the call site (which might not
+ * call the code object in question).
  */
-final class CallArgumentTyper implements DatumProcessor<ModelSite<Call>, Type> {
+final class CalledObjectTyper implements DatumProcessor<ModelSite<Call>, Type> {
 
-	private final InvokableCodeObject invokable;
 	private final SubgoalManager goalManager;
-	private final Variable variable;
 
-	CallArgumentTyper(InvokableCodeObject invokable, Variable variable,
-			SubgoalManager goalManager) {
-		assert variable != null;
-		assert invokable.formalParameters().hasVariableBindingParameter(
-				variable);
+	CalledObjectTyper(SubgoalManager goalManager) {
 		assert goalManager != null;
-
-		this.invokable = invokable;
-		this.variable = variable;
 		this.goalManager = goalManager;
 	}
 
 	@Override
-	public Result<Type> process(final ModelSite<Call> callSite) {
+	public Result<Type> process(ModelSite<Call> callSite) {
 
-		/*
-		 * Which argument is passed to the parameter depends on how this code
-		 * object ends up being invoked by the call site. This a function of the
-		 * type of object being called at the call site (which might not be this
-		 * code object).
-		 * 
-		 * If the callable is a bound method, the arguments are passed to
-		 * different parameters than if it is used unbound. This determination
-		 * is made by the type object of the callable at the callsite so we need
-		 * to infer this type first.
-		 */
-		Result<Type> callableType = goalManager
-				.registerSubgoal(new ExpressionTypeGoal(
-						new ModelSite<exprType>(callSite.astNode().func,
-								callSite.codeObject())));
-
-		/*
-		 * But now that we've got a set of types for the callsite, we don't want
-		 * to include all of them; only those that are implemented by our code
-		 * object.
-		 */
-		return callableType.transformResult(new CallsiteToVariableTypeMapper(
-				callSite, variable, invokable, goalManager));
+		return goalManager.registerSubgoal(new ExpressionTypeGoal(
+				new ModelSite<exprType>(callSite.astNode().func, callSite
+						.codeObject())));
 	}
 };
 
-final class CallsiteToVariableTypeMapper implements
-		Transformer<Type, Result<Type>> {
+/**
+ * Transforms a call-site into the types that may be bound to the given
+ * variable.
+ */
+/*
+ * But now that we've got a set of types for the callsite, we don't want to
+ * include all of them; only those that are implemented by our code object.
+ */
+final class CalledObjectFilter implements Transformer<Type, Result<TCallable>> {
 
-	private final ModelSite<Call> callSite;
 	private final InvokableCodeObject invokable;
 	private final SubgoalManager goalManager;
-	private final Variable variable;
 
-	CallsiteToVariableTypeMapper(ModelSite<Call> callSite, Variable variable,
-			InvokableCodeObject invokable, SubgoalManager goalManager) {
-		assert variable != null;
-		assert invokable.formalParameters().hasVariableBindingParameter(
-				variable);
+	CalledObjectFilter(InvokableCodeObject invokable, SubgoalManager goalManager) {
+		assert invokable != null;
 		assert goalManager != null;
 
-		this.callSite = callSite;
-		this.variable = variable;
 		this.invokable = invokable;
 		this.goalManager = goalManager;
 	}
 
+	private class TopCallable extends Top<TCallable> {
+
+		@Override
+		public String toString() {
+			return "⊤callable";
+		}
+	}
+
 	@Override
-	public Result<Type> transformInfiniteResult() {
+	public Result<TCallable> transformFiniteResult(Set<Type> objectsAtCallSite) {
+
+		Set<TCallable> contributingCallables = new HashSet<TCallable>();
+
+		for (Type calledObject : objectsAtCallSite) {
+
+			if (calledObject instanceof TCallable) {
+
+				TCallable callable = (TCallable) calledObject;
+
+				switch (callingObjectMightInvokeOurCodeObject(callable)) {
+				case YES:
+					contributingCallables.add(callable);
+					break;
+
+				case NO:
+					/*
+					 * calling this callable never results in our code object
+					 * being invoked so it doesn't contribute to the type of the
+					 * parameter's bindings.
+					 */
+					break;
+
+				case NOT_A_CLUE:
+					return new TopCallable();
+
+				default:
+					throw new AssertionError();
+				}
+
+			} else {
+				System.err.println("UNTYPABLE: call site isn't "
+						+ "calling a callable: " + calledObject);
+			}
+		}
+
+		return new FiniteResult<TCallable>(contributingCallables);
+	}
+
+	@Override
+	public Result<TCallable> transformInfiniteResult() {
 		/*
 		 * No idea how we are being called so don't know what our parameter is
 		 */
-		return TopT.INSTANCE;
+		return new TopCallable();
 		/*
 		 * TODO: could take the union of the types of all the arguments passed
 		 * at the callsite as it is only which of them that gets passed that we
@@ -220,95 +335,138 @@ final class CallsiteToVariableTypeMapper implements
 		 */
 	}
 
-	@Override
-	public Result<Type> transformFiniteResult(Set<Type> result) {
-		RedundancyEliminator<Type> type = new RedundancyEliminator<Type>();
-
-		for (Type typeAtCallSite : result) {
-
-			if (typeAtCallSite instanceof TCallable) {
-
-				type.add(typeContributedByCallingCallable((TCallable) typeAtCallSite));
-
-			} else {
-				System.err.println("UNTYPABLE: call site isn't "
-						+ "calling a callable: " + typeAtCallSite);
-			}
-
-			if (type.isFinished()) {
-				break;
-			}
-		}
-
-		return type.result();
+	private enum ObjectIsRelevant {
+		YES, NO, NOT_A_CLUE
 	}
 
-	private Result<Type> typeContributedByCallingCallable(TCallable callable) {
-
-		if (callingObjectMightInvokeOurCodeObject(callable)) {
-
-			uk.ac.ic.doc.gander.flowinference.call.Call call = new DefaultCall(
-					invokable, callable, callSite);
-
-			Result<Argument> arguments = call.argumentsBoundToVariable(
-					variable, goalManager);
-
-			return arguments
-					.transformResult(new Transformer<Argument, Result<Type>>() {
-
-						@Override
-						public Result<Type> transformFiniteResult(
-								Set<Argument> arguments) {
-
-							RedundancyEliminator<Type> parameterType = new RedundancyEliminator<Type>();
-
-							for (Argument argument : arguments) {
-								parameterType.add(argument.type(goalManager));
-
-								if (parameterType.isFinished())
-									break;
-							}
-
-							return parameterType.result();
-						}
-
-						@Override
-						public Result<Type> transformInfiniteResult() {
-							return TopT.INSTANCE;
-						}
-					});
-
-		} else {
-			/*
-			 * calling this callable never results in our code object being
-			 * invoked so it doesn't contribute to the type of the parameter's
-			 * bindings.
-			 */
-			return FiniteResult.bottom();
-		}
-	}
-
-	public boolean callingObjectMightInvokeOurCodeObject(TCallable object) {
+	private ObjectIsRelevant callingObjectMightInvokeOurCodeObject(
+			TCallable object) {
 
 		return object.codeObjectsInvokedByCall(goalManager).transformResult(
-				new Transformer<InvokableCodeObject, Boolean>() {
+				new Transformer<InvokableCodeObject, ObjectIsRelevant>() {
 
 					@Override
-					public Boolean transformFiniteResult(
+					public ObjectIsRelevant transformFiniteResult(
 							Set<InvokableCodeObject> codeObjectsInvokedByCall) {
 
-						return codeObjectsInvokedByCall.contains(invokable);
+						if (codeObjectsInvokedByCall.contains(invokable)) {
+							return ObjectIsRelevant.YES;
+						} else {
+							return ObjectIsRelevant.NO;
+						}
 					}
 
 					@Override
-					public Boolean transformInfiniteResult() {
+					public ObjectIsRelevant transformInfiniteResult() {
 						/*
 						 * We can't tell if this possible receiver of the call
 						 * in question is implemented by the code object whose
 						 * parameter we are looking at.
 						 */
-						return false;
+						return ObjectIsRelevant.NOT_A_CLUE;
 					}
 				});
+	}
+}
+
+final class CallPackager
+		implements
+		Transformer<TCallable, Result<uk.ac.ic.doc.gander.flowinference.call.Call>> {
+	private final ModelSite<Call> senderCallSite;
+	private final InvokableCodeObject invokable;
+
+	CallPackager(InvokableCodeObject invokable, ModelSite<Call> senderCallSite) {
+		this.invokable = invokable;
+		this.senderCallSite = senderCallSite;
+	}
+
+	@Override
+	public Result<uk.ac.ic.doc.gander.flowinference.call.Call> transformFiniteResult(
+			Set<TCallable> relevantCallables) {
+
+		Set<uk.ac.ic.doc.gander.flowinference.call.Call> calls = new HashSet<uk.ac.ic.doc.gander.flowinference.call.Call>();
+
+		for (TCallable callable : relevantCallables) {
+			calls.add(new DefaultCall(invokable, callable, senderCallSite));
+		}
+
+		return new FiniteResult<uk.ac.ic.doc.gander.flowinference.call.Call>(
+				calls);
+	}
+
+	@Override
+	public Result<uk.ac.ic.doc.gander.flowinference.call.Call> transformInfiniteResult() {
+		return new Top<uk.ac.ic.doc.gander.flowinference.call.Call>() {
+
+			@Override
+			public String toString() {
+				return "⊤call";
+			}
+		};
+	}
+}
+
+final class CallDancer implements
+		Transformer<uk.ac.ic.doc.gander.flowinference.call.Call, Result<Type>> {
+
+	private final Variable variable;
+	private final SubgoalManager goalManager;
+
+	public CallDancer(Variable variable, SubgoalManager goalManager) {
+		this.variable = variable;
+		this.goalManager = goalManager;
+	}
+
+	@Override
+	public Result<Type> transformFiniteResult(
+			Set<uk.ac.ic.doc.gander.flowinference.call.Call> calls) {
+
+		RedundancyEliminator<Type> type = new RedundancyEliminator<Type>();
+
+		for (uk.ac.ic.doc.gander.flowinference.call.Call call : calls) {
+
+			Result<Argument> arguments = call.argumentsBoundToVariable(
+					variable, goalManager);
+
+			type.add(arguments.transformResult(new ArgumentTyper(goalManager)));
+			if (type.isFinished())
+				break;
+		}
+
+		return type.result();
+	}
+
+	@Override
+	public Result<Type> transformInfiniteResult() {
+		return TopT.INSTANCE;
+	}
+}
+
+final class ArgumentTyper implements Transformer<Argument, Result<Type>> {
+
+	private final SubgoalManager goalManager;
+
+	public ArgumentTyper(SubgoalManager goalManager) {
+		this.goalManager = goalManager;
+	}
+
+	@Override
+	public Result<Type> transformFiniteResult(Set<Argument> arguments) {
+
+		RedundancyEliminator<Type> type = new RedundancyEliminator<Type>();
+
+		for (Argument argument : arguments) {
+
+			type.add(argument.type(goalManager));
+			if (type.isFinished())
+				break;
+		}
+
+		return type.result();
+	}
+
+	@Override
+	public Result<Type> transformInfiniteResult() {
+		return TopT.INSTANCE;
 	}
 }
